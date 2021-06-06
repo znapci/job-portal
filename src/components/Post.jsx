242a18af8e68ed367dbd6afb773e83da3fd021c6
numannaeem <numan.naeem9@gmail.com>
2021-06-06 20:15:15 +0530

    Minor refactoring

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -8,23 +8,31 @@ const Post = ({ id, title, company, imgUrl, startDate, applyByDate, salary }) =>
     return (
 
         <div className='post' >
-            {id !== null ? <img src={imgUrl} height='20px' width='20px' alt='company logo'></img> : <Skeleton circle={true} height='20px' width='20px'></Skeleton>}
-
-            {
-                id !== null ?
+            { id !== null ? 
+                <>
+                    <img src={imgUrl} height='20px' width='20px' alt='company logo' />
                     <div className='post-company-field' >
                         {company}
-                    </div> : <Skeleton width='20%' />}
-
-            { id !== null ?
-                <div className='post-clickable-content' onClick={() => {
-                    history.push(url)
-                }}><span className='post-title-field'  >
-                        {title}
-                    </span>
-                    <span className='post-startdate-field'>{startDate}</span>
-                    <span className='post-lastdate-field'> {applyByDate}</span>
-                    <span className='post-salary-field'> {salary}</span>  </div> : <Skeleton height='15vh' />}
+                    </div>
+                    <div className='post-clickable-content' onClick={() => {
+                        history.push(url)
+                    }}>
+                        <span className='post-title-field'  >
+                            {title}
+                        </span>
+                        <span className='post-startdate-field'>{startDate}</span>
+                        <span className='post-lastdate-field'> {applyByDate}</span>
+                        <span className='post-salary-field'> {salary}</span>  
+                    </div>
+                </>
+                : 
+                <>
+                    <Skeleton circle={true} height='20px' width='20px' />
+                    <Skeleton width='20%' /> 
+                    <Skeleton width='20%' />
+                    <Skeleton height='15vh' />
+                </>
+            }
         </div>
     )
 }
--- src/components/PostsArea.jsx
+++ src/components/PostsArea.jsx
@@ -5,7 +5,6 @@ import Post from "./Post";
 const PostsArea = ({ apiEndpoint }) => {
     const [state, setState] = useState(Array(5).fill(null));
     const url = apiEndpoint;
-    const posts = [];
 
     useEffect(() => {
         fetch(url, {
@@ -20,8 +19,9 @@ const PostsArea = ({ apiEndpoint }) => {
             });
 
     }, [url]);
-    state.map((post, index) =>
-        posts.push(<Post key={index} id={post ? post.id : null} title={post ? post.title : null} company={post ? post.company : null} imgUrl={post ? post.imgUrl : null} salary={post ? post.salary : null} startDate={post ? post.startDate : null} applyByDate={post ? post.applyByDate : null} />));
+    const posts = state.map((post, index) =>
+        (<Post key={index} id={post ? post.id : null} title={post ? post.title : null} company={post ? post.company : null} imgUrl={post ? post.imgUrl : null} salary={post ? post.salary : null} startDate={post ? post.startDate : null} applyByDate={post ? post.applyByDate : null} />)
+    );
 
     return (
         <div className='posts-area'>
